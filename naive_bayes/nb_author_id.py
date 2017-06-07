bd28d413ac8904c6af31e23c8c518852e4dede78
wookler <dsharma82@gmail.com>
2017-06-07 09:36:16 -0700

    Classifier for Author detection
    simple classifier using Gaussian NB for Author detection

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,38 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+### create a new classifier
+### create predictions based on input test features using the predict function
+### estimate accuracy using accuracy_score function
 #########################################################
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+def predictAuthor():
+	### create a new classifier
+	clf = GaussianNB()
+
+	### start clock
+	t_start_train = time()
+
+	### train the classifier with training features and labels
+	clf.fit(features_train, labels_train)
+
+	###output learning time
+	print "training time:", round(time()-t_start_train, 3), "s"
+
+	t_start_predict = time()
+
+	### create predictions based on input test features using the predict function
+	predictions = clf.predict(features_test)
+
+	print "prediction time:", round(time()-t_start_predict, 3), "s"
+
+	### estimate accuracy using accuracy_score function
+	accuracy = accuracy_score(labels_test, predictions)
+
+	print accuracy
 
 
+predictAuthor()
